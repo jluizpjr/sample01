abaaf815faf2f5d48440d8b1807a1299e8eee12f
Joao Luiz Pereira <jluizpjr@gmail.com>
2021-09-17 11:09:12 -0300

    First version

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import sys
 
 host = os.environ.get('HOSTNAME', 'localhost')
 
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',host=host)
+    return render_template('index.html',host=host,env=os.environ)
