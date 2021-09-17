e7114525bcb75d0e01546eb13fa996368e6ef8ce
Joao Luiz Pereira <jluizpjr@gmail.com>
2021-09-17 13:42:59 -0300

    First version

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from json2html import *
 import os
 import sys
 import json
@@ -14,17 +15,5 @@ app = Flask(__name__)
 def index():
 
 
-#    data_dic = {
-#        'id': [100, 101, 102],
-#        'color': ['red', 'blue', 'red']}
-
-    data_dic = json.dumps({**{}, **os.environ}, indent=2)
-
-    columns = ['key', 'vlue']
-    index = ['a', 'b', 'c']
-
-#    df = pd.DataFrame(data_dic, columns=columns, index=index)
-    df = pd.DataFrame.from_dict(data_dic,columns=columns, index=index)
-    table = df.to_html(index=False)
-
+    table=json2html.convert(json = env)
     return render_template('index.html',host=host,table=table)
