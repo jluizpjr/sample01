fc9d514dafbff7b14470191e0c47c58371dcc7de
Joao Luiz Pereira <jluizpjr@gmail.com>
2021-09-17 11:45:28 -0300

    First version

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask, render_template, request
 import os
 import sys
 import json
+import pandas as pd
 
 host = os.environ.get('HOSTNAME', 'localhost')
-
+env=json.dumps({**{}, **os.environ}, indent=2)
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',host=host,env=json.dumps({**{}, **os.environ}, indent=2))
+
+
+    data_dic = {
+        'id': [100, 101, 102],
+        'color': ['red', 'blue', 'red']}
+    columns = ['id', 'color']
+    index = ['a', 'b', 'c']
+
+    df = pd.DataFrame(data_dic, columns=columns, index=index)
+    table = df.to_html(index=False)
+
+    return render_template('index.html',host=host,table=table)
