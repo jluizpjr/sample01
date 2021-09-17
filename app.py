1fed67146b3e179ee5bce290b8a2204c937a7f95
Joao Luiz Pereira <jluizpjr@gmail.com>
2021-09-17 09:53:18 -0300

    First version

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask, render_template
+import longhorn
+
+longhorn_url = 'http://127.0.0.1:8080/v1'
+
+client = longhorn.Client(url=longhorn_url)
+
+# Volume operations
+# List all volumes
+volumes = client.list_volume()
 
 app = Flask(__name__)
 
